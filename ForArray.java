7994ae87e8d982b33b63b68682724b51177149ea
ThiagoH03 <thiagohenriq03@outlook.com>
2022-03-07 19:50:21 +0000

    Criação do Código

---
+++ ForArray.java
@@ -0,0 +1,10 @@
+public class ForArray {
+    public static void main(String[] args){
+
+        int [] array = {1, 2, 3, 4, 5};
+
+        for(int i : array){
+            System.out.println("Número atual: " + i);
+        }
+    }
+}
